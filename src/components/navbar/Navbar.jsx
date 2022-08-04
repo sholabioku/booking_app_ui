449596b1cb9588c96a298dcbe783e1e0f5b89ac3
sholabioku <bilushonline@gmail.com>
2022-08-04 05:03:05 +0100

    Add jsx for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import './navbar.css';
 
 const Navbar = () => {
-  return <div>Nabvar</div>;
+  return (
+    <div className='navbar'>
+      <div className='navConatiner'>
+        <span className='logo'>bilushbooking</span>
+        <div className='navItems'>
+          <button className='navButton'>Register</button>
+          <button className='navButton'>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
