7e8710a9532c12279cae4e9bae35b587a7e17133
sholabioku <bilushonline@gmail.com>
2022-08-05 11:25:59 +0100

    Add ui design for headerSearch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBed,
   faCar,
+  faPerson,
   faPlane,
   faTaxi,
 } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays } from '@fortawesome/free-regular-svg-icons';
 
 const Header = () => {
   return (
@@ -40,6 +42,27 @@ const Header = () => {
           with a free Lamabooking account
         </p>
         <button className='headerBtn'>Sign in / Register</button>
+        <div className='headerSearch'>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faBed} className='headerIcon' />
+            <input
+              type='text'
+              placeholder='Where are you going?'
+              className='headerSearchInput'
+            />
+          </div>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+            <span className='headerSearchText'>date to date</span>
+          </div>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+            <span className='headerSearchText'>2 adults 2 children 1 room</span>
+          </div>
+          <div className='headerSearchItem'>
+            <button className='headerBtn'>Search</button>
+          </div>
+        </div>
       </div>
     </div>
   );
