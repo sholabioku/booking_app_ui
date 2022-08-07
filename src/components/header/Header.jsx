9bdd66d382c69ff7c9b1fd8870bdac77709f350e
sholabioku <bilushonline@gmail.com>
2022-08-07 18:54:54 +0100

    Add searchItem component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,8 +14,11 @@ import { format } from 'date-fns';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 import './header.css';
+import { useNavigate } from 'react-router-dom';
 
 const Header = ({ type }) => {
+  const navigate = useNavigate();
+  const [destination, setDestination] = useState('');
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
@@ -39,6 +42,10 @@ const Header = ({ type }) => {
         [name]: operation === 'i' ? options[name] + 1 : options[name] - 1,
       };
     });
+  };
+
+  const handleSearch = () => {
+    navigate('/hotels', { state: { destination, date, options } });
   };
 
   return (
@@ -87,6 +94,7 @@ const Header = ({ type }) => {
                   type='text'
                   placeholder='Where are you going?'
                   className='headerSearchInput'
+                  onChange={(e) => setDestination(e.target.value)}
                 />
               </div>
               <div className='headerSearchItem'>
@@ -105,6 +113,7 @@ const Header = ({ type }) => {
                     moveRangeOnFirstSelection={false}
                     ranges={date}
                     className='date'
+                    minDate={new Date()}
                   />
                 )}
               </div>
@@ -183,7 +192,9 @@ const Header = ({ type }) => {
                 )}
               </div>
               <div className='headerSearchItem'>
-                <button className='headerBtn'>Search</button>
+                <button className='headerBtn' onClick={handleSearch}>
+                  Search
+                </button>
               </div>
             </div>
           </>
